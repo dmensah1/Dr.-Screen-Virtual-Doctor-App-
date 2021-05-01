232753abf6390b9ec9e2fa0efb9f98317e556a89
Liam So <Liam.So@dal.ca>
2021-05-01 14:10:15 -0300

    Form added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import { UserProvider } from "./contexts/UserProvider";
 import Login from "./pages/auth/Login";
+import Booking from "./pages/Booking";
 import Home from "./pages/Home";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route path="/login" exact component={Login} />
           <Route path="/" exact component={Home} />
+          <Route path="/booking" exact component={Booking} />
         </Switch>
       </Router>
     </UserProvider>

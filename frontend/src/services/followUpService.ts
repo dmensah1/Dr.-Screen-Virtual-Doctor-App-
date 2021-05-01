a58e84626070113a75d43e1b104ede5d3c365af2
Liam So <Liam.So@dal.ca>
2021-05-01 18:02:50 -0300

    Updated code

---
+++ frontend/src/services/followUpService.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { BACKEND_URL } from '../config/config';
+import { FollowUp } from '../interfaces/Interface';
+
+const URL = `${BACKEND_URL}/followups`;
+
+export const createFollowUp: (details: Partial<FollowUp>) => Promise<{followUp: string}> = async (details: Partial<FollowUp>) => {
+  const res: any = await axios.post(URL, details);
+  return res;
+};
+
+export const getFollowUp: (id: string) => Promise<FollowUp> = async (id: string) => {
+  const res: any = await axios.get(`${URL}/${id}`);
+  return res;
+};
+
+export const getDoctorFollowUps: (doctorId: string) => Promise<FollowUp> = async (doctorId: string) => {
+  const res: any = await axios.get(`${URL}/forDoctor/${doctorId}`);
+  return res;
+};
+
+export const getPatientFollowUps: (patientId: string) => Promise<FollowUp> = async (patientId: string) => {
+  const res: any = await axios.get(`${URL}/forPatient/${patientId}`);
+  return res;
+};
+
+export const deleteFollowUp = async (followUpId: string) => {
+  return await axios.delete(`${URL}/${followUpId}`);
+};
